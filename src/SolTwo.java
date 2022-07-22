4862205ee11bd5fa5030422ffef6b70b33aba2e8
kunal nagwanshi <nagwanshikunal23@gmail.com>
2022-07-22 13:53:42 +0530

    first commit

---
+++ src/SolTwo.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class SolTwo {
+
+        public static void main(String[] args) {
+
+            Scanner in = new Scanner(System.in);
+            int a = in.nextInt();
+
+            double b = in.nextDouble();
+            in.nextLine();
+            String c = in.nextLine();
+            System.out.println("String: "+c);
+            System.out.println("Double: "+b);
+            System.out.println("Int: "+a);
+
+
+        }
+    }
+
